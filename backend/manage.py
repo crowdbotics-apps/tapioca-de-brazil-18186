4d55bd8b67e8f92f4a137d16b52caf7803a47d18
Crowdbotics <team@crowdbotics.com>
2020-06-18 06:36:25 +0000

    Update models

--- backend/manage.py
+++ backend/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tapioca_de_brazil_18186.settings')
+    os.environ.setdefault("DJANGO_SETTINGS_MODULE", "tapioca_de_brazil_18186.settings")
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
@@ -17,5 +17,5 @@ def main():
     execute_from_command_line(sys.argv)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
