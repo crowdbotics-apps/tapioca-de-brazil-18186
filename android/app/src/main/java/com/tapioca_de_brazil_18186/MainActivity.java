9b7267295c5082df8c8582d4e61200b270b55196
Crowdbotics <team@crowdbotics.com>
2020-06-18 06:34:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/tapioca_de_brazil_18186/MainActivity.java
@@ -0,0 +1,15 @@
+package com.tapioca_de_brazil_18186;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "tapioca_de_brazil_18186";
+    }
+}
